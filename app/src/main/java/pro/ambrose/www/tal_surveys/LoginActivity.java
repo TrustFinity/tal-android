651c95e803100331308ea5eb8dceb5ae42ff6ca1
mwakaambrose <ambrosemwaka@gmail.com>
2017-11-09 22:07:02 +0300

    Adds creating user data online.

--- app/src/main/java/pro/ambrose/www/tal_surveys/LoginActivity.java
+++ app/src/main/java/pro/ambrose/www/tal_surveys/LoginActivity.java
@@ -1,28 +1,36 @@
 package pro.ambrose.www.tal_surveys;
 
-import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Build;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Window;
 import android.view.WindowManager;
+
 import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
 import com.facebook.Profile;
-import com.facebook.appevents.AppEventsLogger;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 
 public class LoginActivity extends AppCompatActivity {
 
     private static final String TAG = "LoginTag";
+    public String create_user_url = "http://tal-surveys.herokuapp.com/api/v1/new-respondent";
+    public Profile profile;
+    public String access_token;
     LoginButton loginButton;
     private CallbackManager mCallbackManager;
 
@@ -35,7 +43,7 @@ public class LoginActivity extends AppCompatActivity {
         }
         FacebookSdk.sdkInitialize(getApplicationContext());
         if (Profile.getCurrentProfile() != null){
-            updateUI();
+            handlePostUserData("user already logged in");
         }
         setContentView(R.layout.activity_login);
         getSupportActionBar().hide();
@@ -47,8 +55,7 @@ public class LoginActivity extends AppCompatActivity {
         loginButton.registerCallback(mCallbackManager, new FacebookCallback<LoginResult>() {
             @Override
             public void onSuccess(LoginResult loginResult) {
-                Log.d(TAG, "facebook:onSuccess:" + loginResult.toString());
-                handleFacebookAccessToken(loginResult.getAccessToken());
+                saveUserDataOnline(loginResult.getAccessToken());
             }
 
             @Override
@@ -61,9 +68,6 @@ public class LoginActivity extends AppCompatActivity {
                 Log.d(TAG, "facebook:onError", error);
             }
         });
-        if (Profile.getCurrentProfile() != null){
-            Log.d(TAG, Profile.getCurrentProfile().getProfilePictureUri(720, 500).toString());
-        }
     }
 
     @Override
@@ -72,23 +76,59 @@ public class LoginActivity extends AppCompatActivity {
         mCallbackManager.onActivityResult(requestCode, resultCode, data);
     }
 
-    private void handleFacebookAccessToken(AccessToken accessToken) {
-        Log.d(TAG, accessToken.toString());
-        // create or update user.
-        // start the home activity
-        updateUI();
+    private void saveUserDataOnline(AccessToken accessToken) {
+        profile = Profile.getCurrentProfile();
+        access_token = accessToken.toString();
+        new PostUserData(create_user_url).execute();
     }
 
-    private void updateUI() {
-        Profile profile = Profile.getCurrentProfile();
-        SharedPreferences sharedPref = this.getPreferences(Context.MODE_PRIVATE);
-        SharedPreferences.Editor editor = sharedPref.edit();
-        editor.putString("first_name", profile.getFirstName());
-        editor.putString("picture", profile.getProfilePictureUri(600, 400).toString());
-        editor.putString("middle_name", profile.getMiddleName());
-        editor.putString("last_name", profile.getLastName());
-        editor.commit();
+    public void handlePostUserData(String response) {
         startActivity(new Intent(LoginActivity.this, Home.class));
         finish();
+        Log.d(TAG, response);
+    }
+
+    private class PostUserData extends AsyncTask<String, String, String> {
+
+        OkHttpClient client = new OkHttpClient();
+        private String URL;
+
+        private PostUserData(String URL) {
+            this.URL = URL;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+            RequestBody body = new FormBody.Builder()
+                    .add("facebook_id", access_token)
+                    .add("firstname", profile.getFirstName())
+                    .add("middle_name", profile.getMiddleName())
+                    .add("lastname", profile.getLastName())
+                    .add("image_url", profile.getProfilePictureUri(500, 500).toString())
+                    .build();
+            Request request = new Request.Builder()
+                    .url(this.URL)
+                    .post(body)
+                    .build();
+            Response response;
+            try {
+                response = client.newCall(request).execute();
+                return response.body().string();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            handlePostUserData(s);
+        }
     }
 }
